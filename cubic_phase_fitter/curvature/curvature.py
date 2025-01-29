fix curvature calculation for all coordinates at the same time

cubic_phase_fitter/curvature/curvature.py
@@ -1,24 +1,26 @@
 import numpy as np
 from .derivative import derivative
 
-def nominator(F_x, F_y, F_z, F_xx, F_xy, F_yy, F_yz, F_zz, F_xz):
-    m = np.array([[F_xx, F_xy, F_xz, F_x],
-                  [F_xy, F_yy, F_yz, F_y],
-                  [F_xz, F_yz, F_zz, F_z],
-                  [F_x, F_y, F_z, 0]])
+def nominator(vals):
+    # for each input, m has a slice as:
+    # m = np.array([[F_xx, F_xy, F_xz, F_x],
+    #               [F_xy, F_yy, F_yz, F_y],
+    #               [F_xz, F_yz, F_zz, F_z],
+    #               [F_x, F_y, F_z, 0]])
+    m = np.array([[vals[:, 3], vals[:, 4], vals[:, 8], vals[:, 0]],
+                  [vals[:, 4], vals[:, 5], vals[:, 6], vals[:, 1]],
+                  [vals[:, 8], vals[:, 6], vals[:, 7], vals[:, 2]],
+                  [vals[:, 0], vals[:, 1], vals[:, 2], np.zeros(len(vals[:, 0]))],
+                  ])
 
-    d = np.linalg.det(m)
+    d = np.array([np.linalg.det(i) for i in m.T])
 
     return d
 
-
-def denominator(F_x, F_y, F_z):
-    g = np.array([F_x, F_y, F_z])
-
-    mag_g = np.linalg.norm(g)
-
-    return mag_g ** 4
-
+def denominator(vals):
+    g = np.array([vals[:, 0], vals[:, 1], vals[:, 2]])
+    mag_g = np.array([np.linalg.norm(i)**4 for i in g.T])
+    return mag_g
 
 def curvature(x, y, z, lamb):
     '''
@@ -26,10 +28,10 @@ def curvature(x, y, z, lamb):
 
     the derivative function was worked out by hand. do not do it again, for your own sanity.
     '''
-    vals = derivative(x, y, z, lamb)
+    derivatives = derivative(x, y, z, lamb)
 
-    n = nominator(vals[0], vals[1], vals[2], vals[3], vals[4], vals[5], vals[6], vals[7], vals[8])
-    d = denominator(vals[0], vals[1], vals[2])
+    n = nominator(derivatives)
+    d = denominator(derivatives)
     K = -(n / d)
 
     return K

cubic_phase_fitter/curvature/derivative.py
@@ -9,14 +9,19 @@ def derivative(x, y, z, lamb):
     t5 = -0.0028 * term5(x, y, z, lamb)
     t6 = -0.0021 * term6(x, y, z, lamb)
 
-    F_x_val  = np.array([t1[0], t2[0], t3[0], t4[0], t5[0], t6[0]]).sum()
-    F_y_val  = np.array([t1[1], t2[1], t3[1], t4[1], t5[1], t6[1]]).sum()
-    F_z_val  = np.array([t1[2], t2[2], t3[2], t4[2], t5[2], t6[2]]).sum()
-    F_xx_val = np.array([t1[3], t2[3], t3[3], t4[3], t5[3], t6[3]]).sum()
-    F_xy_val = np.array([t1[4], t2[4], t3[4], t4[4], t5[4], t6[4]]).sum()
-    F_yy_val = np.array([t1[5], t2[5], t3[5], t4[5], t5[5], t6[5]]).sum()
-    F_yz_val = np.array([t1[6], t2[6], t3[6], t4[6], t5[6], t6[6]]).sum()
-    F_zz_val = np.array([t1[7], t2[7], t3[7], t4[7], t5[7], t6[7]]).sum()
-    F_xz_val = np.array([t1[8], t2[8], t3[8], t4[8], t5[8], t6[8]]).sum()
+    stacked_arrs = np.stack((t1, t2, t3, t4, t5, t6))
 
-    return np.array([F_x_val, F_y_val, F_z_val, F_xx_val, F_xy_val, F_yy_val, F_yz_val, F_zz_val, F_xz_val])
+    # this only works with a single coordinate passed, left in for clarity about what's expected
+    # F_x_val  = np.array([t1[0], t2[0], t3[0], t4[0], t5[0], t6[0]]).sum()
+    # F_y_val  = np.array([t1[1], t2[1], t3[1], t4[1], t5[1], t6[1]]).sum()
+    # F_z_val  = np.array([t1[2], t2[2], t3[2], t4[2], t5[2], t6[2]]).sum()
+    # F_xx_val = np.array([t1[3], t2[3], t3[3], t4[3], t5[3], t6[3]]).sum()
+    # F_xy_val = np.array([t1[4], t2[4], t3[4], t4[4], t5[4], t6[4]]).sum()
+    # F_yy_val = np.array([t1[5], t2[5], t3[5], t4[5], t5[5], t6[5]]).sum()
+    # F_yz_val = np.array([t1[6], t2[6], t3[6], t4[6], t5[6], t6[6]]).sum()
+    # F_zz_val = np.array([t1[7], t2[7], t3[7], t4[7], t5[7], t6[7]]).sum()
+    # F_xz_val = np.array([t1[8], t2[8], t3[8], t4[8], t5[8], t6[8]]).sum()
+
+    derivatives_array = np.array([stacked_arrs.T[i].sum(axis=1) for i in range(stacked_arrs.T.shape[0])])
+
+    return derivatives_array  # np.array([F_x_val, F_y_val, F_z_val, F_xx_val, F_xy_val, F_yy_val, F_yz_val, F_zz_val, F_xz_val])

cubic_phase_fitter/fitter.py
@@ -13,29 +13,24 @@ def fitter(terminal_positions, dimensions):
         params.add('trans_a', value=-terminal_positions[:, 0].mean())
         params.add('trans_b', value=-terminal_positions[:, 1].mean())
         params.add('trans_c', value=-terminal_positions[:, 2].mean())
-        params.add('rot_a', value=np.random.uniform(low=-np.pi, high=np.pi, size=(100,)), min=-np.pi, max=np.pi)
-        params.add('rot_b', value=np.random.uniform(low=-np.pi, high=np.pi, size=(100,)), min=-np.pi, max=np.pi)
-        params.add('rot_c', value=np.random.uniform(low=-np.pi, high=np.pi, size=(100,)), min=-np.pi, max=np.pi)
+        params.add('rot_a', value=np.random.uniform(low=-np.pi, high=np.pi), min=-np.pi, max=np.pi)
+        params.add('rot_b', value=np.random.uniform(low=-np.pi, high=np.pi), min=-np.pi, max=np.pi)
+        params.add('rot_c', value=np.random.uniform(low=-np.pi, high=np.pi), min=-np.pi, max=np.pi)
         params.add('scale', value=dimensions[0])
 
         minner = Minimizer(fitfunc, params, fcn_args=(terminal_positions,))
         iter_result = minner.minimize()
 
         counter += 1
-        # print(counter)
         if counter == 5:
             break
 
         # if the residual is too low we get a memory error later for some reason
         if (iter_result.residual.sum() > 1000) or (iter_result.residual.sum() < 20):
-            # print('fit failed!')
             continue
         else:
-            # print('success')
             result = iter_result
             fit_success = True
-    # if counter == 5:
-    #     continue
 
     if fit_success:
         return result