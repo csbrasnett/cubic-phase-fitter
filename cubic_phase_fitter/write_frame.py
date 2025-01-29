25d3c249d8ec02bb3aca10a938b44411b1100b0e
csbrasnett <c.s.brasnett@rug.nl>
2025-01-29 17:45:45 +0100

    sort out file writing

--- cubic_phase_fitter/write_frame.py
+++ cubic_phase_fitter/write_frame.py
@@ -3,7 +3,7 @@ from MDAnalysis import Universe
 
 
 def write_frame(surface, frame_positions, cutting, point_inds, opstr,
-                resindices, atomnames, resnames,
+                resindices, atomnames, resnames, atom_resinds,
                 time):
 
     sp_final = surface[cutting][point_inds]
@@ -13,7 +13,9 @@ def write_frame(surface, frame_positions, cutting, point_inds, opstr,
     new_resids = np.concatenate((resindices,
                                  np.ones(sp_final.shape[0]) * (resindices[-1] + 1)))
     new_names = np.concatenate((atomnames, opstr))
+
     new_resnames = np.concatenate((resnames, np.array(['SUR'])))
+    new_atom_resinds = np.concatenate((atom_resinds, np.array([1])))
 
     u1 = Universe.empty(n_atoms=all_points.shape[0],
                         n_residues=int(new_resids[-1]) + 1,
@@ -23,6 +25,7 @@ def write_frame(surface, frame_positions, cutting, point_inds, opstr,
 
     u1.add_TopologyAttr('names', new_names)
     u1.add_TopologyAttr('resname', new_resnames)
+    u1.add_TopologyAttr('resids', new_atom_resinds)
 
     u1.load_new(all_points - sp_final.min(axis=0))
 
@@ -30,5 +33,5 @@ def write_frame(surface, frame_positions, cutting, point_inds, opstr,
     u1.dimensions = [dim, dim, dim, 90, 90, 90]
 
     ag = u1.select_atoms('all')
-    ag.write(f'frame_{time / 1e6 :.2f}.gro')
+    ag.write(f'frame_{time / 1e6 :.2f}_us.gro')
 
