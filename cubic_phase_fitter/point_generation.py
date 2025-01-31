53c248a2f1a72531a58cd3239fccd43c64f9b092
csbrasnett <c.s.brasnett@rug.nl>
2025-01-31 12:57:30 +0100

    fix exception in point generation

--- cubic_phase_fitter/point_generation.py
+++ cubic_phase_fitter/point_generation.py
@@ -11,19 +11,19 @@ def point_generator(pos, l):
     y_mean = pos[1]
     z_mean = pos[2]
 
-    # define the mgrid from which to generate the points
-    X, Y, Z = np.mgrid[x_mean - (l / 2):x_mean + (l / 2):(l * 1j),
-              y_mean - (l / 2):y_mean + (l / 2):(l * 1j),
-              z_mean - (l / 2):z_mean + (l / 2):(l * 1j)]
+    try:
+        # define the mgrid from which to generate the points
+        X, Y, Z = np.mgrid[x_mean - (l / 2):x_mean + (l / 2):(l * 1j),
+                           y_mean - (l / 2):y_mean + (l / 2):(l * 1j),
+                           z_mean - (l / 2):z_mean + (l / 2):(l * 1j)]
+    except MemoryError:
+        return None
 
     lamb = 1 / l
 
     surf_eq = D_nodal_approx(X, Y, Z, lamb)
-    try:
-        # find vertices on the surface
-        vertices, simplices, normals, values = measure.marching_cubes(surf_eq)
-    except MemoryError:
-        return None
+    # find vertices on the surface
+    vertices, simplices, normals, values = measure.marching_cubes(surf_eq)
 
     # sort out the vertices and append them to the list of coordinates to write to file
     Xp, Yp, Zp = zip(*vertices)
